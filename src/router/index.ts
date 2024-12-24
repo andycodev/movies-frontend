eca05af9b4ddc2d8bed8cdf471c2377320a21959
andyvega <andy.vega@adventistas.org>
2024-12-24 10:40:50 -0500

    routes update

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,9 @@ import { createWebHistory, createRouter } from 'vue-router'
 import MoviesListPage from '@/pages/movies/movies-list/MoviesListPage.vue'
 
 const routes = [
-  { path: '/', redirect: '/movies-list' }, // Redirige la ruta raíz a /movies-list
-  { path: '/movies-list', component: MoviesListPage },
-  { path: '/:pathMatch(.*)*', redirect: '/movies-list' } // Redirige cualquier ruta no definida a /movies-list
+ // { path: '/', redirect: '/' }, // Redirige la ruta raíz a /movies-list
+  { path: '/', component: MoviesListPage },
+  { path: '/:pathMatch(.*)*', redirect: '/' } // Redirige cualquier ruta no definida a /movies-list
 ]
 
 const router = createRouter({
