e2b480df5736f7b9530dd4033e40065af1c5b5c7
andyvega <andy.vega@adventistas.org>
2024-12-31 12:18:00 -0500

    loading add movies

--- src/pages/movies/movies-list/MoviesListPage.vue
+++ src/pages/movies/movies-list/MoviesListPage.vue
@@ -54,48 +54,51 @@
   </v-carousel>
 
   <v-row dense>
-    <v-col v-for="movie in movies" :key="movie.movieId" cols="12" md="3">
-      <v-card class="pa-0 position-relative" color="#952175">
-        <!-- Imagen de fondo con overlay -->
-        <v-img
-          :src="'https://picsum.photos/1920/1080?random=' + movie.movieId"
-          height="100%"
-          class="position-absolute top-0 left-0 right-0 bottom-0"
-          :style="{ opacity: 0.7 }"
-          cover
-        ></v-img>
+    <span v-if="isLoadingMovies">Cargando películas de movies-backend ...</span>
+    <template v-else>
+      <v-col v-for="movie in movies" :key="movie.movieId" cols="12" md="3">
+        <v-card class="pa-0 position-relative" color="#952175">
+          <!-- Imagen de fondo con overlay -->
+          <v-img
+            :src="'https://picsum.photos/1920/1080?random=' + movie.movieId"
+            height="100%"
+            class="position-absolute top-0 left-0 right-0 bottom-0"
+            :style="{ opacity: 0.7 }"
+            cover
+          ></v-img>
 
-        <!-- Overlay oscuro -->
-        <div class="position-absolute top-0 left-0 right-0 bottom-0" style="background: rgba(0, 0, 0, 0.5);"></div>
+          <!-- Overlay oscuro -->
+          <div class="position-absolute top-0 left-0 right-0 bottom-0" style="background: rgba(0, 0, 0, 0.5);"></div>
 
-        <!-- Contenido de la tarjeta -->
-        <div class="d-flex flex-column justify-space-between text-white position-relative z-index-1 pa-3">
-          <v-card-title class="text-h5">
-            {{ movie.title }}
-          </v-card-title>
+          <!-- Contenido de la tarjeta -->
+          <div class="d-flex flex-column justify-space-between text-white position-relative z-index-1 pa-3">
+            <v-card-title class="text-h5">
+              {{ movie.title }}
+            </v-card-title>
 
-          <v-card-subtitle>
-            <v-chip color="amber" variant="flat">
-              {{ movie.year }}
-            </v-chip>
-          </v-card-subtitle>
+            <v-card-subtitle>
+              <v-chip color="amber" variant="flat">
+                {{ movie.year }}
+              </v-chip>
+            </v-card-subtitle>
 
-          <v-card-text>
-            <p class="text-body-2">
-              {{ movie.description }}
-            </p>
-          </v-card-text>
+            <v-card-text>
+              <p class="text-body-2">
+                {{ movie.description }}
+              </p>
+            </v-card-text>
 
-          <v-card-actions>
-            <v-btn
-              class="ms-2"
-              icon="mdi-play"
-              variant="text"
-            ></v-btn>
-          </v-card-actions>
-        </div>
-      </v-card>
-    </v-col>
+            <v-card-actions>
+              <v-btn
+                class="ms-2"
+                icon="mdi-play"
+                variant="text"
+              ></v-btn>
+            </v-card-actions>
+          </div>
+        </v-card>
+      </v-col>
+    </template>
   </v-row>
 </template>
 
